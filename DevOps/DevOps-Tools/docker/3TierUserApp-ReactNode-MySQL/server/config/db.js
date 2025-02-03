f51751c98f21a6c9af1984300c51e2ed767488ab
ibtisam <m.ibtisam.iq@gmail.com>
2025-02-03 19:48:23 +0500

    update

--- DevOps/DevOps-Tools/docker/3TierUserApp-ReactNode-MySQL/server/config/db.js
+++ DevOps/DevOps-Tools/docker/3TierUserApp-ReactNode-MySQL/server/config/db.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'IbtisamOps', // Replace with your actual MySQL password
-  database: 'test_db'
+  //host: 'localhost',
+  //user: 'root',
+  //password: 'IbtisamOps',
+  //database: 'test_db'
+
+  host: process.env.DB_HOST || 'mysql',  // 'mysql' is the service name used in docker-compose.yml
+  user: process.env.DB_USER || 'root',       // MySQL user
+  password: process.env.DB_PASSWORD || 'IbtisamOps', // MySQL password
+  database: process.env.DB_NAME || 'test_db', // MySQL database name
 });
 
 db.connect(err => {
