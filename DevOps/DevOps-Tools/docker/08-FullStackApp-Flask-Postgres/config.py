08855ac9557638179f109de911a119b5f2ebb41a
ibtisam <m.ibtisam.iq@gmail.com>
2025-02-09 13:08:18 +0500

    update

--- DevOps/DevOps-Tools/docker/08-FullStackApp-Flask-Postgres/config.py
+++ DevOps/DevOps-Tools/docker/08-FullStackApp-Flask-Postgres/config.py
@@ -2,7 +2,7 @@ import os
 import secrets
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://ibtisam:ibtisam@db/loveyou_ibtisam'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@db/$POSTGRES_DB'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_hex(16)  # Generate a new secret key each time the app runs
 
